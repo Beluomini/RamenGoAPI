3f73a46e449d1106204b51bf7e6850b73e3adebd
Beluomini <lucasbeluomini1@gmail.com>
2024-05-31 14:03:08 -0300

    FIX: Solving missed Prisma Service provider from entity modules

--- src/broths/broths.module.ts
+++ src/broths/broths.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { BrothsService } from './broths.service';
 import { BrothsController } from './broths.controller';
+import { PrismaService } from 'src/PrismaService';
 
 @Module({
   controllers: [BrothsController],
-  providers: [BrothsService],
+  providers: [BrothsService, PrismaService],
 })
 export class BrothsModule {}
--- src/proteins/proteins.module.ts
+++ src/proteins/proteins.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ProteinsService } from './proteins.service';
 import { ProteinsController } from './proteins.controller';
+import { PrismaService } from 'src/PrismaService';
 
 @Module({
   controllers: [ProteinsController],
-  providers: [ProteinsService],
+  providers: [ProteinsService, PrismaService],
 })
 export class ProteinsModule {}
