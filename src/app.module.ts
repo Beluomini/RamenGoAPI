85c9e68a18097c9cfde5be72a8c53b807e958614
Beluomini <lucasbeluomini1@gmail.com>
2024-05-31 21:34:21 -0300

    FEAT: Add config to use dot env as global

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BrothsModule } from './broths/broths.module';
 import { ProteinsModule } from './proteins/proteins.module';
 import { OrdersModule } from './orders/orders.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [BrothsModule, ProteinsModule, OrdersModule],
+  imports: [BrothsModule, ProteinsModule, OrdersModule, ConfigModule.forRoot()],
   controllers: [],
   providers: [],
 })
