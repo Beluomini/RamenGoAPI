578ffd912d3a99cadcad60d993232ddb1f402146
Beluomini <lucasbeluomini1@gmail.com>
2024-07-30 10:14:24 -0300

    TEST: Starting E2E tests to proteins

--- src/proteins/proteins.module.ts
+++ src/proteins/proteins.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ProteinsService } from './proteins.service';
 import { ProteinsController } from './proteins.controller';
-import { PrismaService } from 'src/PrismaService';
+import { PrismaService } from '../PrismaService';
 
 @Module({
   controllers: [ProteinsController],
